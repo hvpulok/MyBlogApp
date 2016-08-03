9e6493e6ee74bcf2595e050749b3607f40555398
hvpulok <hvpulok@gmail.com>
2016-08-03 14:49:48 -0600

    Add new blog Post route

--- main.py
+++ main.py
@@ -18,8 +18,19 @@ import os
 import webapp2
 import jinja2
 
+# code to initialize google datastore dB
+from google.appengine.ext import db
+
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+
+# Blog DB model
+class Blog(db.Model):
+    title = db.StringProperty(required= True)
+    description = db.TextProperty(required= True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
 
 # Handler class definition to hadle and render html page request
 class Handler(webapp2.RequestHandler):
@@ -41,6 +52,11 @@ class IndexPage(Handler):
     def get(self):
         self.render_main()
 
+    def post(self):
+        newBlogTitle =  self.request.get('title')
+        newBlogDescription =  self.request.get('description')
+        self.write(newBlogTitle)    
+
 
 # Add Blog Page Handler class definition to hadle and render add_blog html page
 class AddBlogPage(Handler):
@@ -50,7 +66,11 @@ class AddBlogPage(Handler):
     def get(self):
         self.render_main()
 
+
+
+
 app = webapp2.WSGIApplication([
     ('/', IndexPage),
-    ('/addblog', AddBlogPage)
+    ('/blog', IndexPage),
+    ('/blog/addblog', AddBlogPage)
 ], debug=True)
