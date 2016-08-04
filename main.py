ae4e9a9d8b1d8a7ae19370d1ca9bfc304cba6fe2
hvpulok <hvpulok@gmail.com>
2016-08-04 14:51:18 -0600

    fix login post route

--- main.py
+++ main.py
@@ -182,6 +182,13 @@ class LoginPage(Handler):
     def get(self):
         self.render("login.html")        
         
+    def post(self):
+        username = self.request.get('username')
+        password = self.request.get('password')
+        checkRememberMe = self.request.get('checkRememberMe')
+        self.write(username)
+        # self.write(password)
+
 
 # >>>>>>>>>>>>>>>>      Route definitions     <<<<<<<<<<<<<<<<<<<<<<<<
 app = webapp2.WSGIApplication([
